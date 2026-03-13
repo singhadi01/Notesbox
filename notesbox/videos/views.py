c03ac5435273d42ae296a174fde4c3bd3558e837
Aditya Singh <140547416+singhadi01@users.noreply.github.com>
2026-03-13 16:27:20 +0530

    Change model name from gemini-2.0-flash to gemini-1.5-flash

--- notesbox/videos/views.py
+++ notesbox/videos/views.py
@@ -185,7 +185,7 @@ def get_gemini_response(input_text):
     }
 
     model = genai.GenerativeModel(
-        model_name="gemini-2.0-flash",
+        model_name="gemini-1.5-flash",
         generation_config=generation_config,
     )
 
